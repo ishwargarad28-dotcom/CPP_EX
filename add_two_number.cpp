b01d0dd568a1ab933734fe11732c553772092499
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-09 21:21:56 +0530

    Add_Two_Number

---
+++ add_two_number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a;
+  int b;
+  cout<<"Enter value of a : ";
+  cin>>a;
+    cout<<"Enter value of b : ";
+  cin>>b;
+  int ans=a+b;
+  cout<<ans;
+}
