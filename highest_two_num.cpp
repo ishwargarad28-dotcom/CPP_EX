6b3ad5ae59c169c8394b9d90b3ac2db82179f449
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-09 21:36:53 +0530

    highest_two_num.CPP

---
+++ highest_two_num.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"enter first num :";
+  cin>>a;
+  cout<<"enter second num :";
+  cin>>b;
+  if(a>b){
+    cout<<"first number is Greater.";
+  }
+  else{
+    cout<<"second number is Greater.";
+  }
+  return 0;
+}
