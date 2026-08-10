e16ef147f33efc89855d6cdc8e9e8e8d96edf8b7
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-10 20:05:57 +0530

    area_of_square.cpp

---
+++ area_of_square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int num;
+  cout<<"enter the number :";
+  cin>>num;
+  int area;
+  area = num*num;
+  cout<<"Area of square :"<<area;
+  return 0;
+}
