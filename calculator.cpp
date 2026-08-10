55b91e915e62d33d99a59ca3140f94930f5a520b
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-10 19:40:48 +0530

    calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"Enter a :";
+  cin>>a;
+  cout<<"Enter b :";
+  cin>>b;
+  float c;
+  c=a+b;
+  cout<<"Sum oF a + b = "<< c<<"\n";
+
+  c=a-b;
+  cout<<"Sub of a - b = "<<c<<"\n";
+  c=a*b;
+  cout<<"Multi of a * b = "<<c<<"\n";
+  c=a/b;
+  cout<<"Divide of a / b = "<<c<<"\n";
+
+  return 0;
+
+}
