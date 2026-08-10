bb8879375a9c09e49d6dad0e57c3e014cef4169a
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-10 20:06:47 +0530

    area_triangle.cpp

---
+++ area_triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float base,height;
+  cout<<"Enter the base :";
+  cin>>base;
+  cout<<"Enter the height :";
+  cin>>height;
+  float area;
+  area=(0.5*height*base);
+  cout<<"area of Triangle :"<<area;
+  return 0;
+}
