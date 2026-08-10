b176ac1cc19391984ede7012531631e657eef1d3
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-10 20:06:19 +0530

    area_reactangle.cpp

---
+++ area_reactangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int length;
+  int width;
+  cout<<"Enter the Length :";
+  cin>>length;
+  cout<<"Enter the width :";
+  cin>>width;
+  int area;
+  area =length*width;
+  cout<<"Area of reactangle is :"<<area;
+  return 0;
+}
