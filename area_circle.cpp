aa37331161bbe9e7030f15f13947442389b2fba5
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-10 20:05:30 +0530

    area_circle.cpp

---
+++ area_circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float num;
+  cout<<"enter the Radius :";
+  cin>>num;
+  float area;
+  area = 3.14*num*num;
+  cout<<"The Circle Radius is :"<<area;
+  return 0;
+}
