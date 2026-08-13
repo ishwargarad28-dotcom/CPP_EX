5ea8d599d6261f64d7a416a1661cde8e6a37ab09
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-13 21:49:42 +0530

    average.cpp

---
+++ average.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float a, b;
+  cout << "Enter the number : ";
+  cin >> a;
+  cout << "Enter the number : ";
+  cin >> b;
+  float avg;
+  avg = (a + b) / 2;
+  cout << "Average of two number : " << avg;
+  return 0;
+}
