6aacedfb557a4ad3958b94479bec83da008c161d
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-17 23:20:24 +0530

    hello.c

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("Enter the Year : ");
+    scanf("%d", &year);
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+    {
+        printf("This is a Leap Year");
+    }
+    else
+    {
+        printf("Not a Leap Year");
+    }
+
+    return 0;
+}
