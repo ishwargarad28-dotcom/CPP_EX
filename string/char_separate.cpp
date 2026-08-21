10bdf0c365663bf1245b55db608d22c77c3b5df9
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-22 00:43:42 +0530

    char_separate.cpp

---
+++ string/char_separate.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  string str="Ishwar Garad ";
+
+  for(int i=0;i<str.length();i++){
+    cout<<str[i]<<" ";
+  }
+  return 0;
+}
